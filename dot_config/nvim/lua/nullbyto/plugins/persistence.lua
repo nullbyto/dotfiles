b0cf6c8b467632d732fdd8b8bfe9764419572abf
nullbyto <nullbyto@gmail.com>
2023-10-07 18:42:35 +0200

    feat(nvim): added persistence plugin

---
+++ dot_config/nvim/lua/nullbyto/plugins/persistence.lua
@@ -0,0 +1,15 @@
+return {
+    "folke/persistence.nvim",
+    event = "BufReadPre", -- this will only start session saving when an actual file was opened
+    opts = {
+        -- add any custom options here
+        -- restore the session for the current directory
+        vim.keymap.set("n", "<leader>qs", [[<cmd>lua require("persistence").load()<cr>]], { desc = "Restore session for current dir" });
+
+        -- restore the last session
+        vim.keymap.set("n", "<leader>ql", [[<cmd>lua require("persistence").load({ last = true })<cr>]], { desc = "Restore last session" });
+
+        -- stop Persistence => session won't be saved on exit
+        vim.keymap.set("n", "<leader>qd", [[<cmd>lua require("persistence").stop()<cr>]], { desc = "Stop persistence" });
+    },
+}

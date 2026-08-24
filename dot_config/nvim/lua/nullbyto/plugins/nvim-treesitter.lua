bb94196670b4f910c34b7371012fec0301d6efe0
Amin Dieter Hatib <nullbyto@gmail.com>
2026-08-25 00:03:32 +0200

    nvim: update treesitter

--- dot_config/nvim/lua/nullbyto/plugins/nvim-treesitter.lua
+++ dot_config/nvim/lua/nullbyto/plugins/nvim-treesitter.lua
@@ -5,7 +5,8 @@ return {
 		local configs = require("nvim-treesitter.configs")
 
 		configs.setup({
-			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "rust", "go", "toml", "bash", "python", "typescript", "ron" },
+			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "rust"
+                , "go", "toml", "bash", "python", "typescript", "ron", "markdown" },
 			sync_install = false,
 			highlight = { enable = true },
 			indent = { enable = true },  

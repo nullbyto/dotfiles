a743cb14d1bbe5b28922d99b723b149b81aab1e3
Amin Dieter Hatib <nullbyto@gmail.com>
2026-08-27 21:16:46 +0200

    nvim: fix treesitter error on start

--- dot_config/nvim/lua/nullbyto/plugins/nvim-treesitter.lua
+++ dot_config/nvim/lua/nullbyto/plugins/nvim-treesitter.lua
@@ -2,9 +2,9 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	config = function () 
-		local configs = require("nvim-treesitter.configs")
+		local config = require("nvim-treesitter.config")
 
-		configs.setup({
+		config.setup({
 			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "rust"
                 , "go", "toml", "bash", "python", "typescript", "ron", "markdown" },
 			sync_install = false,
